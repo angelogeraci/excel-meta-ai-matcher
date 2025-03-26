53b8b636b41656c889a248dc6fb0d1319b9fdd10
angelogeraci <ageraci.finance@gmail.com>
2025-03-26 11:43:27 +0100

    Ajout de la configuration Vite avec alias et proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,11 +11,16 @@ export default defineConfig({
     },
   },
   server: {
+    port: 3000,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
-      },
-    },
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   },
+  build: {
+    outDir: 'build'
+  }
 })
